3e48378131763881d6cc353695ae0e3d9dc352cf
Vijayakumar, Vulchi (External) <Vulchi.Vijayakumar@xerox.com>
2025-10-01 19:25:34 +0530

    adding some test cases

--- src/App.test.jsx
+++ src/App.test.jsx
@@ -1,12 +1,58 @@
-// src/__tests__/App.test.jsx
 import React from 'react';
-import { render, screen } from '@testing-library/react';
+import { render, screen, fireEvent, waitFor } from '@testing-library/react';
 import App from './App'; // Assuming your main component is App.jsx
 
-describe('App', () => {
-  test('renders the main heading', () => {
+describe("App Component", () => {
+  test("Renders static text Vite + React + Jest", () => {
+    // Arrange
     render(<App />);
-    const headingElement = screen.getByText(/Vite \+ React/i);
+    // Action
+    // Asset
+    const headingElement = screen.getByText("Vijaya Kumar", { exact: false });
     expect(headingElement).toBeInTheDocument();
   });
+  test("Renders default text", () => {
+    // Arrange
+    render(<App />);
+    // Actions
+    // Asset
+    const textElement = screen.getByText("Vite + React", { exact: true });
+    expect(textElement).toBeInTheDocument();
+  });
+  test("Renders text change", () => {
+    // Arrange
+    render(<App />);
+    // Actions
+    const buttonElement = screen.getByRole("button")
+    fireEvent.click(buttonElement);
+    // Asset
+    const textElement = screen.getByText("React + Vite", { exact: true });
+    expect(textElement).toBeInTheDocument();
+  });
+  test("Renders text change again", () => {
+    // Arrange
+    render(<App />);
+    // Actions
+    const btnElement = screen.getByRole("button")
+    fireEvent.click(btnElement);
+    // Asset
+    setTimeout(() => {
+      const textElement = screen.getByText("Vite + React", { exact: true });
+      expect(textElement).toBeInTheDocument();
+    }, 0);
+  });
+  test("Render input field", () => {
+    // Arrange
+    render(<App />)
+    // Actions
+    const inputElement = screen.getByPlaceholderText("Enter name");
+    expect(inputElement).toBeInTheDocument();
+  });
+  test("Render input field is empty", () => {
+    render(<App />);
+    const inputElement = screen.getByPlaceholderText("Enter name");
+    inputElement.setAttribute("value", "vijay");
+    console.log(inputElement.getAttribute("value") === "vijay");
+    expect(inputElement).toBeInTheDocument();
+  })
 });
